2adf4c17fe56be0e7267c3e8a7cbb02419e2448e
Otavio Cunha <otaviocunha@hotmail.com>
2025-01-21 13:19:10 -0500

    Task 8 done

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
+const axios = require('axios');
+
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
@@ -50,8 +52,41 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.session.authorization.username
+    const reviewText = req.body.review
+    const isbn = req.params.isbn; // Substitua pelo ISBN que você deseja consultar
+    const url = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`;
+    
+    axios.get(url)
+    .then (resposta =>{
+        if(resposta.data.items && resposta.data.items.length >0){
+            const titulo=resposta.data.items[0].volumeInfo.title.toLowerCase();
+            
+            const book = Object.values(books).find(book => book.title.toLowerCase() === titulo);
+
+            if (!book) {
+            return res.status(404).json({ message: "Livro não encontrado."});
+            }
+
+            const existingReviewId = Object.keys(book.reviews).find( id => book.reviews[id].username === username); //Test if there's a review for req.user
+
+            if (existingReviewId){
+                book.reviews[existingReviewId].review=reviewText;
+                res.json({message: "Review editada com sucesso!"});
+            }
+            else{
+                const reviewId = Object.keys(book.reviews).length + 1;
+                book.reviews[reviewId] = { username, review: reviewText };
+                res.json({message: "Review adicionada com sucesso!"});
+            }
+        }
+        else {
+            res.status(404).json({ message: "Livro não encontrado na API do Google" });
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ message: "Erro ao acessar a API do Google", error: error.message });
+    });
 });
 
 module.exports.authenticated = regd_users;
