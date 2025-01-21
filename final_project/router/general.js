f1c4fa4ea0999dd597d1f223c170a0ea2e220dca
Otavio Cunha <otaviocunha@hotmail.com>
2025-01-21 11:52:36 -0500

    task6 done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,10 +6,41 @@ const public_users = express.Router();
 
 const axios = require('axios');
 
+const doesExist = (username) => {
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password ){
+        return res.status(404).json({message: "Please, provide a username and a password"});
+    }
+
+    // Check if both username and password are provided
+    if (username && password) {
+        // Check if the user does not already exist
+        if (!doesExist(username)) {
+            // Add the new user to the users array
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
@@ -26,7 +57,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
     .then (resposta =>{
         if(resposta.data.items && resposta.data.items.length >0){
             const titulo=resposta.data.items[0].volumeInfo.title.toLowerCase();
-            console.log(titulo);
+            //console.log(titulo);
             const MyDataOnBook = Object.values(books).filter(book => book.title.toLowerCase() === titulo);
             if (MyDataOnBook.length > 0) {
                 res.json(MyDataOnBook[0]); // Retorna o livro encontrado
